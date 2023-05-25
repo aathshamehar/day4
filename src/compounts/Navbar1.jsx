0916d7253b73a08ab96112295f59d6dc7c48254e
aathshamehar <aathsha@gmail.com>
2023-05-25 12:12:34 +0530

    fourth commit

--- src/compounts/Navbar1.jsx
+++ src/compounts/Navbar1.jsx
@@ -8,9 +8,9 @@ function ColorSchemesExample() {
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="contacts">Contacts</Nav.Link>
             <Nav.Link href="login">Login</Nav.Link>
             <Nav.Link href="about">About</Nav.Link>
