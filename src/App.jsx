76f62dc877f0f26f599bb28bd27846473867e895
aathshamehar <aathsha@gmail.com>
2023-05-25 11:15:43 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import React from 'react'
+import Home from './pages/Home'
+import About from './pages/About'
+import Login from './pages/Login'
+import Contacts from './pages/Contacts'
 
+export default function App() {
+  return (
+    <div>       
+        <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/contacts" element={<Contacts />} />
+   
+ </Routes>
+ </div>
+  )
+}
