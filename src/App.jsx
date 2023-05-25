2a8ae829d97f5d145dc8a953d72716c0676a9aae
aathshamehar <aathsha@gmail.com>
2023-05-25 12:01:02 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'
 import React from 'react'
-import Home from './pages/Home'
-import About from './pages/About'
-import Login from './pages/Login'
-import Contacts from './pages/Contacts'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Login from './Pages/Login'
+import Contacts from './Pages/Contacts'
+import Notfound from './Pages/Notfound'
+import Navbar1 from './compounts/Navbar1'
 
 export default function App() {
   return (
-    <div>       
+    
+    <div>      
+        <Navbar1/> 
         <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/about" element={<About />} />
     <Route path="/login" element={<Login />} />
     <Route path="/contacts" element={<Contacts />} />
-   
+    <Route path="*" element={<Notfound />} />
+
  </Routes>
  </div>
   )
